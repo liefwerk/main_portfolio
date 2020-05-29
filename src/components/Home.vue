955542e0be0c75e52a780f608c79c020f2621dee
mrclasse <natjageni@gmail.com>
2020-05-29 15:31:04 -0300

    Initial commit

---
+++ src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <h1 class="display-2">Hello</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class Home extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import Vue from "vue";
+
+import { BootstrapVue, BootstrapVueIcons } from "bootstrap-vue";
+
+import "bootstrap/dist/css/bootstrap.css";
+
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
 import App from "./App.vue";
 import store from "./store";
+
+Vue.use(BootstrapVue);
+Vue.use(BootstrapVueIcons);
 
 Vue.config.productionTip = false;
 
 new Vue({
   store,
-  render: h => h(App)
+  // eslint-disable-next-line
+  render: (h) => h(App),
 }).$mount("#app");
