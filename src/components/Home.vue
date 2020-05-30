58cebf451485e8cb336a74486cc6bc36a53b10be
mrclasse <natjageni@gmail.com>
2020-05-30 14:52:42 -0300

    included fecthed data to the front end

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,17 +1,39 @@
 <template>
   <div class="container">
-    <h1 class="display-2">Hello</h1>
+    <h1 class="display-2">Hello {{ this.user.name }}</h1>
   </div>
 </template>
 
-<script lang="ts">
+<script lang="js">
 import { Component, Prop, Vue } from "vue-property-decorator";
+import axios from "axios";
 
 @Component
 export default class Home extends Vue {
-  @Prop() private msg!: string;
+  data() {
+    return {
+      user: "",
+    };
+  }
+  async created() {
+    const config = {
+      headers: {
+        Accept: "application/json",
+        crossdomain: true,
+        AccessControlAllowOrigin: "*",
+      },
+    };
+    try {
+      const instance = axios.create({
+        baseURL: "http://127.0.0.1:3000",
+      });
+      const res = await instance.get("/users");
+      this.user = res.data[1];
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
 </script>
 
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
